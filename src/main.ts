16e590197ce86db42681640a59f5da9c90e0a245
bitomic <35199700+bitomic@users.noreply.github.com>
2021-11-02 20:32:04 -0600

    feat: sync sequelize before start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import '@sapphire/plugin-logger/register'
-import { Client, env } from './lib'
+import { Client, env, sequelize } from './lib'
 import { container } from '@sapphire/framework'
 
 ( async () => {
+	await sequelize.sync()
 	const client = new Client()
 	try {
 		await client.login( env.DISCORD_TOKEN )
