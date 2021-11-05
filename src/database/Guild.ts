73366f5a8ae9d7375eebd42a34928a16249b7e35
bitomic <35199700+bitomic@users.noreply.github.com>
2021-11-05 14:43:18 -0600

    fix(database): better guild model

--- src/database/Guild.ts
+++ src/database/Guild.ts
@@ -3,15 +3,21 @@ import type { Model } from 'sequelize'
 import { sequelize } from '../lib'
 
 interface IGuild {
-	language: string
+	language?: string
+	limit?: number
 	snowflake: string
 }
 
-interface IGuildInterface extends Model<IGuild, IGuild>, IGuild {
+interface IGuildInterface extends Model<IGuild, IGuild>, Required<IGuild> {
 }
 
 export const Guild = sequelize.define<IGuildInterface>( 'Guild', {
 	language: {
+		defaultValue: 'en-US',
+		type: DataTypes.STRING
+	},
+	limit: {
+		defaultValue: 1,
 		type: DataTypes.STRING
 	},
 	snowflake: {
