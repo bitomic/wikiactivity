c72abe9da8ab8b30868b7043aa250a3fa60415e1
bitomic <35199700+bitomic@users.noreply.github.com>
2021-11-05 14:54:31 -0600

    feat: utility methods for slash commands

--- src/framework/SlashCommand.ts
+++ src/framework/SlashCommand.ts
@@ -1,4 +1,4 @@
-import type { ApplicationCommandData, ApplicationCommandOptionData, CommandInteraction } from 'discord.js'
+import type { ApplicationCommandData, ApplicationCommandOptionData, CommandInteraction, InteractionReplyOptions } from 'discord.js'
 import type { Awaitable } from '@sapphire/utilities'
 import { Piece } from '@sapphire/framework'
 import type { PieceContext } from '@sapphire/framework'
@@ -21,6 +21,42 @@ export abstract class SlashCommand extends Piece {
 	}
 
 	public abstract run( interaction: CommandInteraction ): Awaitable<unknown>
+
+	protected async canManage( interaction: CommandInteraction ): Promise<boolean> {
+		if ( !this.isGuild( interaction ) ) return false
+		let permissions = interaction.memberPermissions
+		if ( !permissions ) {
+			const guild = interaction.guild ?? await this.container.client.guilds.fetch( interaction.guildId )
+			const member = await guild.members.fetch( interaction.user.id )
+			// eslint-disable-next-line prefer-destructuring
+			permissions = member.permissions
+		}
+
+		const hasPermission = permissions.has( 'MANAGE_GUILD' )
+		if ( !hasPermission ) {
+			this.reply( interaction, {
+				content: `You need the \`Manage server\` permission to use this command.`
+			} )
+		}
+		return hasPermission
+	}
+
+	protected isGuild( interaction: CommandInteraction ): interaction is CommandInteraction<'present'> {
+		const inGuild = interaction.inGuild()
+		if ( !inGuild ) {
+			this.reply( interaction, {
+				content: `This command can only be used in guilds.`
+			} )
+		}
+		return inGuild
+	}
+
+	protected reply( interaction: CommandInteraction, options: InteractionReplyOptions ): void {
+		const method = interaction.replied || interaction.deferred
+			? 'editReply'
+			: 'reply'
+		void interaction[ method ]( options )
+	}
 }
 
 export type SlashCommandOptions = ApplicationCommandData & {
