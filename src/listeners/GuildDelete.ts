76d6ac167d29612bb2e99d8ec94dfcc83573a7f7
bitomic <35199700+bitomic@users.noreply.github.com>
2021-11-05 15:44:27 -0600

    feat(listeners): delete from database on guild delete

---
+++ src/listeners/GuildDelete.ts
@@ -0,0 +1,25 @@
+import { Guild, Wiki }  from '../database'
+import { ApplyOptions } from '@sapphire/decorators'
+import { Constants } from 'discord.js'
+import type { GatewayGuildDeleteDispatch } from 'discord-api-types/v9'
+import { Listener } from '@sapphire/framework'
+import type { ListenerOptions } from '@sapphire/framework'
+
+@ApplyOptions<ListenerOptions>( {
+	event: Constants.Events.GUILD_DELETE
+} )
+export class UserEvent extends Listener {
+	public async run( guild: GatewayGuildDeleteDispatch[ 'd' ] ): Promise<void> {
+		if ( guild.unavailable ) return
+		await Wiki.destroy( {
+			where: {
+				guild: guild.id
+			}
+		} )
+		await Guild.destroy( {
+			where: {
+				snowflake: guild.id
+			}
+		} )
+	}
+}
