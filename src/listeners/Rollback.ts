77a078aebb40008bdeef9d264586825502816437
bitomic <35199700+bitomic@users.noreply.github.com>
2022-04-02 14:01:46 -0600

    debug: log rollback token

--- src/listeners/Rollback.ts
+++ src/listeners/Rollback.ts
@@ -34,7 +34,7 @@ export class UserEvent extends Listener {
 				const bot = await this.container.client.getFandomBot()
 				if ( bot ) {
 					const token = await this.container.client.wiki.getToken( 'rollback' ) as unknown as string
-					console.log( await this.container.client.wiki.post( {
+					console.log( token, await this.container.client.wiki.post( {
 						action: 'rollback',
 						markbot: true,
 						title,
