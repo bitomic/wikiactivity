24d985762ad6117bdc6c87c00f8c5e2a25945541
bitomic <35199700+bitomic@users.noreply.github.com>
2022-06-19 18:58:36 -0600

    fix: emit join event when socket connects

--- src/events/Connection.ts
+++ src/events/Connection.ts
@@ -5,7 +5,16 @@ import { ApplyOptions } from '@sapphire/decorators'
 	event: 'connection'
 } )
 export class UserEvent extends Event {
-	public run() {
+	public async run(): Promise<void> {
 		this.container.pino.info( 'WebSocket connected successfully!' )
+
+		const configurations = this.container.stores.get( 'models' ).get( 'configurations' )
+		const wikis = [ ...await configurations.getWikis() ]
+		if ( wikis.length === 0 ) {
+			this.container.pino.info( 'No wikis are registered. The socket won\'t join any room.' )
+			return
+		}
+		this.container.io.send( 'join', wikis )
+		this.container.pino.info( `Joined the following rooms: ${ wikis.join( ', ' ) }` )
 	}
 }
--- src/listeners/Ready.ts
+++ src/listeners/Ready.ts
@@ -7,16 +7,7 @@ import { Constants } from 'discord.js'
 	once: true
 } )
 export class UserEvent extends Listener {
-	public async run(): Promise<void> {
+	public run(): void {
 		this.container.pino.info( 'Client is ready and running.' )
-
-		const configurations = this.container.stores.get( 'models' ).get( 'configurations' )
-		const wikis = [ ...await configurations.getWikis() ]
-		if ( wikis.length === 0 ) {
-			this.container.pino.info( 'No wikis are registered. The socket won\'t join any room.' )
-			return
-		}
-		this.container.io.send( 'join', wikis )
-		this.container.pino.info( `Joined the following rooms: ${ wikis.join( ', ' ) }` )
 	}
 }
