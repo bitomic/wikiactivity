3453fe8cd3ec918f1cf10372b316ba30610f42ba
bitomic <35199700+bitomic@users.noreply.github.com>
2022-06-19 21:51:52 -0600

    fix: exempt me from precondition

--- src/preconditions/ManageServer.ts
+++ src/preconditions/ManageServer.ts
@@ -1,13 +1,14 @@
 import { Precondition, type PreconditionOptions, type PreconditionResult } from '@sapphire/framework'
 import { ApplyOptions } from '@sapphire/decorators'
 import type { CommandInteraction } from 'discord.js'
+import { env } from '../lib';
 
 @ApplyOptions<PreconditionOptions>( {
 	name: 'ManageServer'
 } )
 export class UserPrecondition extends Precondition {
 	public override chatInputRun( interaction: CommandInteraction ): PreconditionResult {
-		return interaction.memberPermissions?.has( 'MANAGE_GUILD' )
+		return interaction.memberPermissions?.has( 'MANAGE_GUILD' ) || interaction.user.id === env.DISCORD_OWNER
 			? this.ok()
 			: this.error()
 	}
